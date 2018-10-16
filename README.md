06c142c09439106616a819fe84e7793c8d5a94af
emicklei_kramphub <ernest@kramphub.com>
2018-10-16 15:44:32 +0200

    initial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # protobuf2map
+
+protobuf2map can decode a ProtocolBuffer marshaled message into a map[string]interface{} using its .proto definition.
+
+This package was created to transform such messages directly to JSON without using the generated (Go) code to do the unmarshaling and marshaling. It can be used to inspect messages for quick view and debugging. Do not use this if performance is of importance.
---
+++ definitions_test.go
@@ -0,0 +1,18 @@
+package protobuf2map
+
+import "testing"
+
+func TestAddFromFile(t *testing.T) {
+	d := NewDefinitions()
+	d.AddFromFile("test.proto")
+	if got, want := len(d.filenamesRead), 1; got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+	m, ok := d.Message("protobuf2map", "Test")
+	if !ok {
+		t.Fail()
+	}
+	if got, want := m.Name, "Test"; got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
